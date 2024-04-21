9e22776f64914264866e7a11baa751be7304cdc9
Corey Graham <142066414+Wintersgate@users.noreply.github.com>
2024-04-20 21:54:24 -0500

    Created divs for grid

---
+++ javascript.js
@@ -0,0 +1,8 @@
+const container = document.querySelector('#container');
+
+for (i = 0; i < 256; i++) {
+    const content = document.createElement('div');
+    content.classList.add('content');
+
+    container.appendChild(content);
+}
